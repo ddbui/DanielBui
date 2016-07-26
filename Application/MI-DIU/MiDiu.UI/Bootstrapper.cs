145032aed7dbd5ea1b1b1c20f95a8aec9d8f5653
ddbui <danielbui@yahoo.com>
2016-07-26 14:37:34 -0700

    Added bootstrapper and shell (or main window)

---
+++ Application/MI-DIU/MiDiu.UI/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using MiDiu.UI.Views;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+using System.Windows;
+
+namespace MiDiu.UI
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+        }
+
+
+    }
+}
