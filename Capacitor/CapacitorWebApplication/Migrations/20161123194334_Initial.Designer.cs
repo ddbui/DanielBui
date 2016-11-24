95e644b695b21b9a3b7576f7f1905d8f4d5cd3a4
ddbui <danielbui@yahoo.com>
2016-11-23 17:25:35 -0800

    Added Bootstrap

---
+++ Capacitor/CapacitorWebApplication/Migrations/20161123194334_Initial.Designer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using CapacitorWebApplication.Models;
+
+namespace CapacitorWebApplication.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20161123194334_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("CapacitorWebApplication.Models.Resin", b =>
+                {
+                    b.Property<int>("ResinId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ResinId");
+
+                    b.ToTable("Resins");
+                });
+        }
+    }
+}
---
+++ Capacitor/CapacitorWebApplication/Migrations/20161123194334_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CapacitorWebApplication.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Resins",
+                columns: table => new
+                {
+                    ResinId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resins", x => x.ResinId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Resins");
+        }
+    }
+}
