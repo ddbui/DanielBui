cc2d66295556b32c059c12915feede6f1a2b3eab
ddbui <danielbui@yahoo.com>
2016-11-24 07:58:33 -0800

    Added Material.cs

---
+++ Capacitor/CapacitorWebApplication/Models/Material.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapacitorWebApplication.Models
+{
+    public class Material
+    {
+        public int MaterialId { get; set; }
+        public string Name { get; set; }
+    }
+}
