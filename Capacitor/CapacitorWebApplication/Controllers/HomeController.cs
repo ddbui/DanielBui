b7abb0d96b223865927de17d72d7fcf9db13be79
ddbui <danielbui@yahoo.com>
2016-12-21 11:17:14 -0800

    Deleted some obsolete projects

--- Capacitor/CapacitorWebApplication/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace CapacitorWebApplication.Controllers
-{
-    public class HomeController : Controller
-    {
-        public ViewResult Index() => View("Index");
-    }
-}
--- Capacitor/CapacitorWebApplication/Models/Repositories/IResinRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace CapacitorWebApplication.Models
-{
-    public interface IResinRepository
-    {
-        IEnumerable<Resin> Resins { get; }
-    }
-}
