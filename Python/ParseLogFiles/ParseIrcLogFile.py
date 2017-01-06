a5561e0f07f61c1d5545cd21116a9f59dcc8f352
ddbui <danielbui@yahoo.com>
2017-01-05 18:24:19 -0800

    n/a

--- Python/ParseLogFiles/ParseIrcLogFile.py
+++ Python/ParseLogFiles/ParseIrcLogFile.py
@@ -5,87 +5,135 @@ Spyder Editor
 This is a temporary script file.
 """
 #!/usr/bin/env python3
+import datetime
 
-def process(line, dict):     
-    trainings = ("pluralsight", "infiniteskills", "teamtreehouse", "digitaltutors", "ostrainings", "tutsplus", "learnnowonline")
+def process(line, dict2, search_items):
     
-    # Search for those with "0x" and exclude them
-    if not "0x" in line and "#" in line:
-        line2 = line.replace("[ ", "[")
-        #print(line2)
+    search_terms = ("pluralsight", "lynda", "tutsplus", "infiniteskills")
+    
+    # Not a line that we want to parse        
+    if not "#" in line:
+        return
         
-        # Need to do something like try... catch...
-        try:
-            month, date, time, user, slot, number, size, title = line2.split()
-            user = user.replace("<", "")
-            user = user.replace(">", "")
-            title = title.replace(".tar", "")
-            title = title.replace(".", " ")
+    test_line = line.lower()        
+
+    # There are some materials that we're not interested in!
+    none_available  = "0x" in test_line
+    hdtv            = "hdtv" in test_line or "[mg]-tv" in test_line
+    bdrip           = "bdrip" in test_line
+    non_english     = "german" in test_line
+    dvdrip          = "dvdrip" in test_line and not "xxx" in test_line
+    xvid            = "xvid" in test_line and not "xxx" in test_line
+    divx            = "divx" in test_line and not "xxx" in test_line
+    webrip          = "webrip" in test_line and not "xxx" in test_line
+    webdl           = "web-dl" in test_line and not "xxx" in test_line
+    
+    if (none_available or hdtv or bdrip or non_english or 
+        dvdrip or xvid or divx or webrip or webdl):
+        return
+
+    line = line.replace("[ ", "[")    
+
+    try:
+        words = line.split()
+        num_words = len(words)
+        
+        if num_words < 8:
+            return
             
-            words = title.split()           
-
-            dict[title] = "/msg {} xdcc send {}, {} {}".format(user, slot, size, number)
-                
-        except Exception:
-            #print(line)
-            pass    
+        number = words[5]
+        number = number.replace("x", "")
+        if number == "0":
+            return
+            
+        slot = words[4]
+            
+        user = words[3]
+        user = user.replace("<", "")
+        user = user.replace(">", "")
         
-        return dict
+        size = words[6]
         
-def write_to_file(keys, dict):        
+        title = ''.join(words[7:num_words])                   
+        title = title.replace(".tar", "")
+        title = title.replace(".", " ")
+        displayed_title = title
+        title = title.replace("-", " ")
+        title = title.lower()
+        
+        dict2[displayed_title] = "{} {}, /msg {} xdcc send {}".format(size, number, user, slot)
+        
+        for term in search_terms:
+            if term in title:
+                search_items[displayed_title] = "{} {}, /msg {} xdcc send {}".format(size, number, user, slot)
+            
+    except Exception:
+        print("EXCEPTION: ", line)
+        pass    
+    
+    return dict2
+        
+# Writes results to an output file
+def write_to_file(input_file, channel, date, keys, dict2):        
     keys.sort()       
-       
-    with open("hexchat_output.txt", "w", encoding="utf8") as outfile:
-        for key in keys:
-            #print(key, dict[key])
-            outfile.write(key + " " + dict[key] + "\n")
+
+    output_file = "hexchat_output_{0}_{1}.txt".format(channel, date)
+    with open(output_file, "w", encoding="utf8") as outfile:
+        outfile.write(input_file + "\n")
         
-def main():      
-    
-    # Open and read a text file
-    count = 0
-    dict = {}
-    with open ("C:/Users/Daniel/AppData/Roaming/HexChat/logs/Abjects/#beast-xdcc.log", encoding="utf8") as f:
-        for line in f:
-            count += 1
-            tmp = line.lower()
+        for key in keys:            
+            
+            try:
+                outfile.write(key + " " + dict2[key] + "\n")
 
-            #training
-#            if (not "pluralsight"       in tmp and
-#                not "infiniteskills"    in tmp and
-#                not "teamtreehouse"     in tmp and
-#                not "digitaltutors"     in tmp and
-#                not "ostrainings"       in tmp and
-#                not "tutsplus"          in tmp and
-#                not "learnnowonline"    in tmp and
-#                not "lesson"            in tmp and
-#                not "lynda"             in tmp):
-                
-            #software                
-#            if (not "keymaker"          in tmp and
-#                not "keygen"            in tmp):
-    
-            #if not "hdtv" in tmp:                
-
-#            if not "bluray" in tmp or not "201" in tmp:
-                
-            if not ".xxx." in tmp:                                
+            except Exception:
+                print("EXCEPTION: ", key)
                 continue
             
-            #year 2010 and after
-#            if not "201" in tmp:
-#                continue
-            
-            process(line, dict)            
+def process_file(network, channel, date, search_items):
+    count = 0
+    dict2 = {}
+    
+    input_file = "C:\\Users\\Daniel\\AppData\\Roaming\\HexChat\\logs\\{0}\\{1}\\#{2}.log".format(network, date, channel)
+    
+    with open (input_file, encoding="utf8") as f:
+        # for loop in python is slow, so avoid it if we can!
+        for line in f:
+            count += 1
+            process(line, dict2, search_items)            
             
     print("processed " + str(count) + " lines")            
-    keys = list(dict.keys())    
-    write_to_file(keys, dict)
+    keys = list(dict2.keys())    
+    write_to_file(input_file, channel, date, keys, dict2)            
+        
+# Main function
+def main():
+    
+    # Open and read a text file
+    now = datetime.datetime.now()
+    date = "{0}-{1:02d}-{2:02d}".format(now.year, now.month, now.day)
+    
+    search_items = {}
+    
+    process_file("Abjects", "beast-xdcc", date, search_items)
+    process_file("Abjects", "MOVIEGODS", date, search_items)
+    process_file("Criten", "ELITEWAREZ", date, search_items)
+    process_file("Abandoned", "ZOMBIE-WAREZ", date, search_items)
+    
+    keys = list(search_items.keys())
+    keys.sort()
+    output_file = "hexchat_output_search_items.txt"
+    with open(output_file, "w", encoding="utf8") as outfile:        
+        for key in keys:
             
-#    for key, value in dict.items():
-#        print(key, value)
-        
-if __name__ == "__main__":
-    main()        
+            try:
+                outfile.write(key + " " + search_items[key] + "\n")
 
-        
+            except Exception:
+                print("EXCEPTION: ", key)
+                continue    
+
+
+# This is one of the best practices!
+if __name__ == "__main__":
+    main()
