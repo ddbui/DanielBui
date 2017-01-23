10e2d0ca9f0d1866918c4c175193c4ed8accd40f
ddbui <danielbui@yahoo.com>
2017-01-23 07:13:08 -0800

    n/a

--- Python/ParseLogFiles/ParseIrcLogFile.py
+++ Python/ParseLogFiles/ParseIrcLogFile.py
@@ -9,7 +9,9 @@ import datetime
 
 def process(line, dict2, search_items):
     
-    search_terms = ("pluralsight", "lynda", "tutsplus", "infiniteskills")
+    search_terms = ("pluralsight", "lynda", "tutsplus", "infiniteskills",
+                    "ebook",
+                    "bluray")
     
     # Not a line that we want to parse        
     if not "#" in line:
@@ -27,9 +29,13 @@ def process(line, dict2, search_items):
     divx            = "divx" in test_line and not "xxx" in test_line
     webrip          = "webrip" in test_line and not "xxx" in test_line
     webdl           = "web-dl" in test_line and not "xxx" in test_line
+    french_shit     = "l.equipe" in test_line or "la.libre.belgique" in test_line or \
+                      "la.gazzetta" in test_line or "le.figaro" in test_line or \
+                      "le.monde" in test_line or "le.parisien" in test_line or "les.echos" in test_line
     
     if (none_available or hdtv or bdrip or non_english or 
-        dvdrip or xvid or divx or webrip or webdl):
+        dvdrip or xvid or divx or webrip or webdl or
+        french_shit):
         return
 
     line = line.replace("[ ", "[")    
@@ -61,11 +67,11 @@ def process(line, dict2, search_items):
         title = title.replace("-", " ")
         title = title.lower()
         
-        dict2[displayed_title] = "{} {}, /msg {} xdcc send {}".format(size, number, user, slot)
+        #dict2[displayed_title] = "{} {}, /msg {} xdcc send {}".format(size, number, user, slot)
         
         for term in search_terms:
             if term in title:
-                search_items[displayed_title] = "{} {}, /msg {} xdcc send {}".format(size, number, user, slot)
+                search_items["{}: {}".format(term, displayed_title)] = "{} {}, /msg {} xdcc send {}".format(size, number, user, slot)
             
     except Exception:
         print("EXCEPTION: ", line)
@@ -115,19 +121,42 @@ def main():
     
     search_items = {}
     
+    process_file("Abandoned", "PORN-HQ", date, search_items)
+    process_file("Abandoned", "warez", date, search_items)
+    process_file("Abandoned", "ZOMBIE-WAREZ", date, search_items)
+    
     process_file("Abjects", "beast-xdcc", date, search_items)
     process_file("Abjects", "MOVIEGODS", date, search_items)
+    
+    #process_file("AlphaIrc", "masterwarez", date, search_items)
+    
+    process_file("Criten", "0day-mp3s", date, search_items)
+    process_file("Criten", "Batcave", date, search_items)
     process_file("Criten", "ELITEWAREZ", date, search_items)
-    process_file("Abandoned", "ZOMBIE-WAREZ", date, search_items)
+    process_file("Criten", "masterwarez", date, search_items)
+    process_file("Criten", "the-future", date, search_items)
+    
+    process_file("Frozyn", "BLACKMARKET-MUSIC", date, search_items)
+    process_file("Frozyn", "blackmarket-warez", date, search_items)
+    
+    process_file("Iarec", "ULTRA-WAREZ", date, search_items)
+    
+    process_file("Rizon", "ELITEWAREZ", date, search_items)
+    
+    process_file("Scenep2p", "THE.SOURCE", date, search_items)
+    
+    process_file("Xerologic", "mp3", date, search_items)
+    process_file("Xerologic", "warez", date, search_items)
+    
     
     keys = list(search_items.keys())
     keys.sort()
-    output_file = "hexchat_output_search_items.txt"
+    output_file = "Output\\hexchat_output_search_items.txt"
     with open(output_file, "w", encoding="utf8") as outfile:        
         for key in keys:
             
             try:
-                outfile.write(key + " " + search_items[key] + "\n")
+                outfile.write("{}, {}\n".format(key, search_items[key]))
 
             except Exception:
                 print("EXCEPTION: ", key)
